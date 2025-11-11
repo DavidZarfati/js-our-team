56540c745c9ed4f5c11ff9c4260da8866a73e3af
DavidZarfati <zarda2001@gmail.com>
2025-11-11 17:00:30 +0100

    feat: dopo correzione con loris, divisione in data.js e function.js

---
+++ function.js
@@ -0,0 +1,47 @@
+function createSingleCard(member) {
+    const { name, role, img } = member;
+    const card = `
+  <div class="team-card col-12 col-sm-6 col-md-4 col-lg-3 ">
+    <div class="card-img">
+      <img
+        src="${img}"
+        alt="${name}"
+        style="width:200px; height:200px; object-fit:cover;"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${name}</h3>
+      <p>${role}</p>
+    </div>
+  </div>
+  `;
+    return card;
+}
+function printCardsGrid() {
+    const teamContainer = document.querySelector(".team-container");
+    let cardString = ""
+    for (let i = 0; i < teamMembers.length; i++) {
+        const curMember = teamMembers[i];
+        const { name, role, img } = curMember;
+        const card = createSingleCard(curMember)
+        cardString += card
+    }
+    teamContainer.innerHTML += cardString
+}
+printCardsGrid()
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const newMember = {
+        name: nameInput.value,
+        role: roleInput.value,
+        img: imgInput.value,
+    }
+    teamMembers.push(newMember);
+    const teamContainer = document.querySelector(".team-container");
+    const newCard = createSingleCard(newMember);
+    teamContainer.innerHTML += newCard;
+    nameInput.value = '';
+    roleInput.value = '';
+    imgInput.value = '';
+})
+
